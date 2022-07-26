df145aba673f8a6e8f444d71a90f2a0b64e73f95
Daniel <daniel.sprcp@gmail.com>
2022-07-26 10:55:47 -0300

    Exercicio 3 finalizado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,25 @@
 //Crie a const para a frase aqui
+
+const frase = ("Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"")
+
+console.log(frase)
+
+// ITEM B: SUBSTITUINDO TERMOS:
+
+const novaFrase = frase.replaceAll("verde","rosa").replaceAll("azul","laranja")
+
+console.log(novaFrase)
+
+// ITEM C: VERIFICAR SE NOVA STRING INCLUI VERDE E LARANJA:
+
+const temVerde = novaFrase.includes("verde")
+const temLaranja = novaFrase.includes("laranja")
+
+console.log("A nova string inclui verde?", temVerde)
+console.log("A nova string inclui laranja?", temLaranja)
+
+// EXTRA:
+
+const fraseMaiuscula = frase.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")
+
+console.log(fraseMaiuscula)
