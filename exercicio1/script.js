fcfcb78ec0196451b8b369c04b3742e3595e659d
Daniel <daniel.sprcp@gmail.com>
2022-07-26 10:11:05 -0300

    exercicio 1 finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+// EXERCICIO 1:
+
+// PERGUNTANDO AO USUARIO O SEU NOME E QUAIS SUAS COMIDAS FAVORITAS:
+
+const nome = prompt("Qual é o seu nome?")
+const comida1 = prompt("Qual sua comida favorita 1?")
+const comida2 = prompt("Qual sua comida favorita 2?")
+const comida3 = prompt("Qual sua comida favorita 3?")
+
+// IMPRIMINDO LISTA DE COMIDAS NA TELA:
+
+console.log(`Estas são as comidas favoritas de ${nome}: \n${comida1} \n${comida2} \n${comida3}`)
